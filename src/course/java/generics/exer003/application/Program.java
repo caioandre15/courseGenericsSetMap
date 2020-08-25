4460d53192adcc94d6b675616d4eb8b2a751303d
Caio André <caio_andre15@hotmail.com>
2020-08-25 20:29:13 -0300

    Exercise 3 modified

--- src/course/java/generics/exer003/application/Program.java
+++ src/course/java/generics/exer003/application/Program.java
@@ -1,15 +1,20 @@
 package course.java.generics.exer003.application;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
-
 	public static void main(String[] args) {
-		List<?> myObjs = new ArrayList<Object>();
-		List<Integer> myNumbers = new ArrayList<Integer>();
-		myObjs = myNumbers; // erro de compilação
-
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+		
+		List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
+		printList(myStrs);
 	}
 
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+	}
 }
