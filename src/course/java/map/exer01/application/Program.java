769b57aaca342ae1100d8d90495841a5489a82b2
Caio André <caio_andre15@hotmail.com>
2020-09-07 17:43:40 -0300

    Map exercise 01

---
+++ src/course/java/map/exer01/application/Program.java
@@ -0,0 +1,31 @@
+package course.java.map.exer01.application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Map<String, String> cookies = new TreeMap<>();
+		
+		cookies.put("username", "Maria");
+		cookies.put("email", "maria@gmail.com");
+		cookies.put("phone", "99711122");
+		
+		cookies.remove("email");
+		cookies.put("phone", "995959");
+		
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
+		
+		System.out.println("ALL COOKIES:");
+		for (String key : cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
+		}
+		
+	}
+
+}
